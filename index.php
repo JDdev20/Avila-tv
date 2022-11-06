a66869d0e3d389741ef1f576452f0e6d2046a808
Plateurs <josehlpv@gmail.com>
2022-11-06 03:46:33 -0400

    comencé con el login pero me perdí con algunos comandos marvada sea

--- index.php
+++ index.php
@@ -6,23 +6,16 @@
     <div class="inicio">
         <div class="barraLogo">
             <img src="assets/logos/Logo-avilatv.png" id="avilaLogo"" alt="">
-
-            <input type="id" name="cedula" placeholder="Ingrese su identificación..." id="inputCedula">
-            <div class="links">
-                <a href="visitantes.php">Visitante</a>
-                <a href="login.php">Iniciar sesión</a>
-            </div>
-
             <input type="number" name="cedula" placeholder="Ingrese su identificación..." id="inputCedula">
         </div>
         <div class="links">
-            <a href="#">Visitante</a>
-            <a href="#">Iniciar sesión</a>
+            <a href="visitantes.php">Visitante</a>
+            <a href="login.php">Iniciar sesión</a>
 
         </div>
     </div>
 </div>
     
-    <!--Incluyebdo el FOOTER-->
+    <!--Incluyedo el FOOTER-->
 <?php include('includes/footer.php'); ?>
 
--- login.php
+++ login.php
@@ -2,19 +2,24 @@
 
 <!--Incluyebdo el HEADER-->
 <?php include('includes/header.php'); ?>
+
 <div class="body">
     <div class="inicio">
-        <div class="barraLogo">
-            <img src="assets/logos/Logo-avilatv.png" id="avilaLogo"" alt="">
-            <input type="id" name="cedula" placeholder="Ingrese su identificación..." id="inputCedula">
-            <div class="links">
-                <a href="#">Visitante</a>
-                <a href="#">Iniciar sesión</a>
+        <div class="contenido">
+            <div class="img-container"></div>
+            <div class="form-container">
+                <h1>Bienvenido de vuelta.</h1>
+                <p>Inicia sesión</p>
+                <form class="formulario">
+                    <input type="text" name="Usuario" id="usuario_id" value="Usuario">
+                    <input type="password" name="Contraseña" placeholder="Contraseña" id="password_id">
+                </form>
+                <div class="consultor">
+                    <a href="#" class="consultor_id">Consultar...</a>
+                </div>
             </div>
         </div>
     </div>
 </div>
-    
-    <!--Incluyebdo el FOOTER-->
+<!--Incluyedo el FOOTER-->
 <?php include('includes/footer.php'); ?>
-
