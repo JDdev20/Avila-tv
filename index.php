f424572f014ec5a21fa6b4a36bbf53cb4fbec441
Plateurs <josehlpv@gmail.com>
2022-11-05 21:06:36 -0400

    arreglados los logos

--- index.php
+++ index.php
@@ -2,11 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Avila-tv</title>
 </head>
 <body>
     
+    <div class="inicio">
+
+    </div>
+
 </body>
 </html>
