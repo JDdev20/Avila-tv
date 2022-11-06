7bc7ee06e9fa256b3b50fc7ceb8b3fa16605d35f
Plateurs <josehlpv@gmail.com>
2022-11-06 00:48:51 -0400

    arreglé los links

--- index.php
+++ index.php
@@ -1,27 +1,17 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/styles.css">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Avila-TV</title>
 
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/dash.css">
-</head>
-<body>
+<?php include('includes/header.php'); ?>
+
     
     <div class="inicio">
         <div class="barraLogo">
             <img src="assets/logos/Logo-avilatv.png" id="avilaLogo"" alt="">
             <input type="id" name="cedula" placeholder="Ingrese su identificación..." id="inputCedula">
-            <div class="links">
+        </div>
+        <div class="links">
             <a href="#">Visitante</a>
             <a href="#">Iniciar sesión</a>
-            </div>
         </div>
     </div>
-</body>
-</html>
+
+    <?php include('includes/footer.php'); ?>
 
