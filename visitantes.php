16c3d0116b64d8070ee43522706084fa5a9a0dd8
Plateurs <josehlpv@gmail.com>
2022-11-08 23:22:45 -0400

    Tratando de hacer el formulario de visitante, ya tengo el concepto, solo me falta arreglar ese codigo

--- visitantes.php
+++ visitantes.php
@@ -6,10 +6,29 @@
     <div class="inicio">
         <div class="barraLogo">
             <img src="assets/logos/Logo-avilatv.png" id="avilaLogo"" alt="">
-            <input type="id" name="cedula" placeholder="Ingrese su identificación..." id="inputCedula">
-            <div class="links">
-                <a href="#">Visitante</a>
-                <a href="#">Iniciar sesión</a>
+            <div class="grupo_inputs">
+                <div class="input">
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" name="nombre" placeholder="Ingrese su nombre..." id="" required>
+                </div>
+                <div class="input">
+                    <label for="apellido">Apellido:</label>
+                    <input type="text" name="apellido" placeholder="Ingrese su apellido..." id="" required>
+                </div>
+                <div class="input">
+                    <label for="cedula">Cédula:</label>
+                    <input type="text" name="cedula" placeholder="Ingrese su cedula..." id="" required>
+                </div>
+                <div class="input">
+                    <label for="equipo">Equipo:</label>
+                    <input type="text" name="equipo" placeholder="Ingrese el código del equipo..." id="" required>
+                </div>
+                <div class="input">
+                    <input type="submit" value="Enviar" class="enviar">
+                </div>
+                <div class="links">
+                    <a href="index.php">Volver.</a>
+                </div>
             </div>
         </div>
     </div>
